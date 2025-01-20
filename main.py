ab9be84739db31b43d8bf5c68ca50aa1a05006fa
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2025-01-20 20:44:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import datetime
+import pandas
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pprint import pprint
+from collections import defaultdict
+
+
+def year_shape_generator(age_difference):
+    delta_new = age_difference % 100
+    if 21 > delta_new > 4:
+        return "лет"
+    delta_new = age_difference % 10
+    if delta_new == 1:
+        return "год"
+    elif 1 < delta_new < 5:
+        return "года"
+    return "лет"
+
+
+def get_information_excel_table():
+    excel_wine_2 = pandas.read_excel('wine3.xlsx', na_values=' ', keep_default_na=False)
+    wines = excel_wine_2.to_dict(orient='records')
+    sorted_wines = defaultdict(list) 
+
+    for wine in wines:
+        sorted_wines[wine['Категория']].append(wine)
+
+    category_order = ['Белые вина', 'Красные вина', 'Напитки']
+    ordered_wines = {}
+    
+    for category in category_order:
+        if category in sorted_wines:
+            ordered_wines[category] = sorted_wines[category]
+
+    return ordered_wines
+
+
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    date_creation = 1920
+    today_date = datetime.datetime.now()
+
+    age_difference = today_date.year - date_creation
+
+    year_shape = year_shape_generator(age_difference)
+    wines = get_information_excel_table()
+
+    rendered_page = template.render(
+        age_of_wine=age_difference,
+        year=year_shape,
+        wines=wines
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
